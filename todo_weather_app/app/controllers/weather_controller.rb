0cffe62f97ed36ed4c8fed5e2a6102c60cbc4ffd
Mawande Nyoni <nyonimawande@gmail.com>
2025-12-05 18:52:35 +0200

    Add caching for API call

--- todo_weather_app/app/controllers/weather_controller.rb
+++ todo_weather_app/app/controllers/weather_controller.rb
@@ -9,15 +9,21 @@ class WeatherController < ApplicationController
     lon = params[:lon] || 18.42535499728242
     api_key = Rails.application.credentials.weather_api_key
 
-    current_uri = URI("http://api.weatherapi.com/v1/current.json?key=#{api_key}&q=#{lat},#{lon}")
-    astronomy_uri = URI("http://api.weatherapi.com/v1/astronomy.json?key=#{api_key}&q=#{lat},#{lon}")
+    current_res = fetch_cached_data("temp", "http://api.weatherapi.com/v1/current.json?key=#{api_key}&q=#{lat},#{lon}")
+    astronomy_res = fetch_cached_data("astro", "http://api.weatherapi.com/v1/astronomy.json?key=#{api_key}&q=#{lat},#{lon}")
 
-    current_res = Net::HTTP.get(current_uri)
-    astronomy_res = Net::HTTP.get(astronomy_uri)
     temp_c = JSON.parse(current_res)["current"]["temp_c"]
     sunrise = JSON.parse(astronomy_res)["astronomy"]["astro"]["sunrise"]
     sunset = JSON.parse(astronomy_res)["astronomy"]["astro"]["sunset"]
 
     render json: { temp_c: temp_c, sunrise: sunrise, sunset: sunset }
+  end
+
+  def fetch_cached_data(data_name, url)
+    cache_key = "WEATHER_DATA_#{data_name}"
+    Rails.cache.fetch(cache_key, expires_in: 1.hour) do
+      uri = URI(url)
+      Net::HTTP.get(uri)
+    end
   end
 end
